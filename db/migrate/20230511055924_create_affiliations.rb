0071e137334385471e7c4517368f8f975515b8b9
adusu-takano <adusu-takano@aroma.ocn.ne.jp>
2023-06-30 16:00:42 +0900

    index修正

--- db/migrate/20230511055924_create_affiliations.rb
+++
@@ -1,9 +0,0 @@
-class CreateAffiliations < ActiveRecord::Migration[6.1]
-  def change
-    create_table :affiliations do |t|
-      t.string :affiliation_name
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20230512233141_create_working_safety_matters.rb
+++
@@ -1,9 +0,0 @@
-class CreateWorkingSafetyMatters < ActiveRecord::Migration[6.1]
-  def change
-    create_table :working_safety_matters do |t|
-      t.string :working_safety_matter_name
-
-      t.timestamps null: false
-    end
-  end
-end
