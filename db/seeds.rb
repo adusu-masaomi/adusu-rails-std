9b4c07ec102a9dac28bf7ead163c14f05e97a10f
adusu-takano <adusu-takano@aroma.ocn.ne.jp>
2023-07-03 18:52:05 +0900

    seed対応1852

--- db/seeds.rb
+++ db/seeds.rb
@@ -82,8 +82,22 @@ require "csv"
 #end
 
 #render
-CSV.foreach('db/affiliations.csv') do |row|
-  Affiliation.create(:affiliation_name => row[0])
+#CSV.foreach('db/affiliations.csv') do |row|
+#  Affiliation.create(:affiliation_name => row[0])
+#end
+
+#render
+CSV.foreach('db/companies.csv') do |row|
+  Company.create(:name => row[0], :representative_title => row[1], :representative_name => row[2], :post => row[3], :address => row[4] ,
+                 :house_number => row[5], :tel => row[6], :fax => row[7], :invoice_number => row[8] )
+end
+
+#render
+CSV.foreach('db/working_times.csv') do |row|
+  Company.create(:working_time_start_time => row[0], :working_time_end_time => row[1], :overtime_start_time => row[2], :overtime_end_time => row[3], 
+                 :overtime_early_start_time => row[4] ,overtime_early_end_time => row[5], :overtime_midnight_start_time => row[6], :overtime_midnight_end_time => row[7], 
+                 :break_time_1_start_time => row[8], :break_time_1_end_time => row[9], :break_time_2_start_time => row[10], :break_time_2_end_time => row[11], 
+                 :break_time_3_start_time => row[12], :break_time_3_end_time => row[13])
 end
 
 ######################
