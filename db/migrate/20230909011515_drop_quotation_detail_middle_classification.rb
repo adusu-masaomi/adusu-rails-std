a6646ad135c302157e443c50666bc11c81e6fd95
adusu-takano <adusu-takano@aroma.ocn.ne.jp>
2023-09-09 16:17:46 +0900

    seed9-5

--- db/migrate/20230909011515_drop_quotation_detail_middle_classification.rb
+++ db/migrate/20230909011515_drop_quotation_detail_middle_classification.rb
@@ -1,4 +1,4 @@
-class DropQuoationDetailMiddleClassification < ActiveRecord::Migration[6.1]
+class DropQuotationDetailMiddleClassification < ActiveRecord::Migration[6.1]
   def change
     drop_table :quotation_detail_middle_classifications
   end
