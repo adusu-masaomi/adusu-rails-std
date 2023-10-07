76b68575e63e847169398ded53066f566b550cef
adusu-takano <adusu-takano@aroma.ocn.ne.jp>
2023-10-07 17:56:14 +0900

    id_set補完2

--- db/migrate/20231007043839_id_set_outsourcing_costs.rb
+++ db/migrate/20231007043839_id_set_outsourcing_costs.rb
@@ -1,4 +1,4 @@
-class IdSetConstructionCosts < ActiveRecord::Migration[6.1]
+class IdSetOutsourcingCosts < ActiveRecord::Migration[6.1]
   def change
       execute "SELECT setval('outsourcing_costs_id_seq', 1051)"
   end
