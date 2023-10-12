13f97cc9a3fd96fdcb5e6b013f47923f98e5d3e2
adusu-takano <adusu-takano@aroma.ocn.ne.jp>
2023-10-12 11:02:30 +0900

    sendgrid update1

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -15,8 +15,8 @@ class ApplicationMailer < ActionMailer::Base
   
   #for centos6(220209) 
   #default from:     "株式会社アデュース<adusudenki@gmail.com>"
-  #default from:     @company_name + "<postmailerddxz@gmail.com>"
-  default from:     @company_name + "<adusudenki@gmail.com>"
+  default from:     @company_name + "<postmailerddxz@gmail.com>"
+  #default from:     @company_name + "<adusudenki@gmail.com>"
   
   #↑カッコのメアドがないと555のエラーが出て送信できないので注意！！
   #        reply_to: "sample+reply@gmail.com"
