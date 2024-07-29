2f1d9d571a2bf1c38d731f48e4ec36cdcd65559f
adusu-takano <adusu-takano@aroma.ocn.ne.jp>
2024-07-29 16:06:28 +0900

    メール送信問題対処2

--- app/controllers/quotation_material_headers_controller.rb
+++ app/controllers/quotation_material_headers_controller.rb
@@ -898,8 +898,9 @@ class QuotationMaterialHeadersController < ApplicationController
     if params[:quotation_material_header][:sent_flag] == "1" || params[:quotation_material_header][:sent_flag] == "2" 
       if $seq_exists > 0
       #昇順(編集時)になっている場合は、明細パラメータを本来の降順にしておく。
-        params[:quotation_material_header][:quotation_material_details_attributes] = 
-          Hash[params[:quotation_material_header][:quotation_material_details_attributes].sort.reverse]
+        #DEL240729 応急処置
+        #params[:quotation_material_header][:quotation_material_details_attributes] = 
+        #  Hash[params[:quotation_material_header][:quotation_material_details_attributes].sort.reverse]
       end
   
       #メール送信フラグ(明細用)をセット＆データ更新
