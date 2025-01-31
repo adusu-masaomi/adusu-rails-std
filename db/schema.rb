7899f3cd9bb25dbf3f22f83cd5a8b713a0049f59
adusu-takano <adusu-takano@aroma.ocn.ne.jp>
2025-01-31 09:51:09 +0900

    作業責任者非表示対応

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_17_090023) do
+ActiveRecord::Schema.define(version: 2025_01_31_003102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -883,6 +883,7 @@ ActiveRecord::Schema.define(version: 2024_09_17_090023) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "is_resign"
+    t.integer "is_invisible"
   end
 
   create_table "stocktakes", force: :cascade do |t|
