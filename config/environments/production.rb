dd8d643c66351580e3f73e725632a61d885b94be
adusu-takano <adusu-takano@aroma.ocn.ne.jp>
2025-07-28 10:13:26 +0900

    sendgrid期限切対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,11 +7,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:               'smtp.sendgrid.net',
+    address:               'smtp.postmarkapp.com',
     port:      	           587,
     domain:                'smtp.gmail.com',
     user_name:             'apikey',
-    password:              ENV['SENDGRID_APIKEY'],
+    password:              ENV['POSTMARK_APIKEY'],
     authentication:        'plain',
    enable_starttls_auto: true
   }
